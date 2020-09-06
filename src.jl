24ffd10509e5b73ccc4460204bdb0e15f35a335f
Azamat Berdyshev <aberdysh@gmail.com>
2020-09-05 21:06:03 -0600

    major revision

--- src.jl
+++ src.jl
@@ -4,6 +4,18 @@ using Flux
 using DiffEqFlux
 using GalacticOptim
 
+using CUDA
+CUDA.allowscalar(false)
+
+function NeuralPDE.PhysicsInformedNN(dx, chain::Chain, initθ; autodiff=false, strategy = GridTraining(), kwargs...)
+   indim = size(nnet.layers[1].W, 2)
+   outdim = size(chain.layers[end].W, 1)
+   isuinplace = outdim == 1
+   φ = get_phi(chain, isuinplace)
+   derivative = get_derivative(indim, φ, autodiff, isuinplace)
+   PhysicsInformedNN(dx, chain, initθ, φ, autodiff, derivative, strategy, kwargs)
+end
+
 function fitPDESystem!(K, α, µ, ρ₀, ρ₀ˢ, ρ₀ˡ,
                        F₁, F₂,
                        tspan, x₁span, x₂span,
@@ -11,7 +23,16 @@ function fitPDESystem!(K, α, µ, ρ₀, ρ₀ˢ, ρ₀ˡ,
                        maxiters=1000, strategy = GridTraining())
 
    @parameters t, x₁, x₂, θ
-   @variables u₁(..), u₂(..), v₁(..), v₂(..), σ₁₁(..), σ₁₂(..), σ₂₂(..), p(..)
+
+    u₁ = Variable{Float32}(:u₁)
+    u₂ = Variable{Float32}(:u₂)
+    v₁ = Variable{Float32}(:v₁)
+    v₂ = Variable{Float32}(:v₂)
+   σ₁₁ = Variable{Float32}(:σ₁₁)
+   σ₁₂ = Variable{Float32}(:σ₁₂)
+   σ₂₂ = Variable{Float32}(:σ₂₂)
+     p = Variable{Float32}(:p)
+
    @derivatives Dt'~t
    @derivatives Dx₁'~x₁
    @derivatives Dx₂'~x₂
@@ -79,23 +100,28 @@ function fitPDESystem!(K, α, µ, ρ₀, ρ₀ˢ, ρ₀ˡ,
    domain = [t ∈ tspan, x₁ ∈ x₁span, x₂ ∈ x₂span]
 
    # Neural network
-   nnet = FastChain(FastDense(3,  64, Flux.selu),
-                    FastDense(64, 64, Flux.σ),
-                    FastDense(64, 64, Flux.σ),
-                    FastDense(64, 8))
+   nnet = FastChain(FastDense(3,  16, Flux.selu),
+                    FastDense(16, 16, Flux.σ),
+                    FastDense(16, 16, Flux.σ),
+                    FastDense(16, 8)) #|> gpu
+   # nnet = Chain(Dense(3,  16, Flux.selu),
+   #              Dense(16, 16, Flux.σ),
+   #              Dense(16, 16, Flux.σ),
+   #              Dense(16, 8)) #|> gpu
 
    # Initial neural network training parameters
-   initθ = initial_params(nnet) |> gpu
+   initθ = initial_params(nnet) #|> gpu
 
    discretization = PhysicsInformedNN([Δt, Δx₁, Δx₂], nnet, initθ; strategy)
 
    pde_system = PDESystem(equations, boundconds, domain, [t, x₁, x₂], [u₁, u₂, v₁, v₂, σ₁₁, σ₁₂, σ₂₂, p])
 
    problem = NeuralPDE.discretize(pde_system, discretization)
-   optimizer = Flux.ADAM(0.05)
+
+   optimizer = Flux.ADAM(0.1f0)
    callback = function (p, l)
-       println("Current loss is: $l")
-       return false
+      println("Current loss is: $l")
+      return false
    end
 
    res = GalacticOptim.solve(problem, optimizer; progress=false, cb=callback, maxiters)
@@ -108,7 +134,7 @@ function fitPDESystem!(K, α, µ, ρ₀, ρ₀ˢ, ρ₀ˡ,
    end
 end
 
-# specify constants
+# Specify constants
 const K, α, µ, ρ₀, ρ₀ˢ, ρ₀ˡ = let
    ρ₀ᶠˢ = 1400
    ρ₀ᶠˡ = 997
@@ -125,27 +151,27 @@ const K, α, µ, ρ₀, ρ₀ˢ, ρ₀ˡ = let
    α₃ = (cp₁^2 + cp₂^2 - 8/3*ρ₀ˢ/ρ₀*cs^2 - √((cp₁^2 - cp₂^2)^2 - 64/9*cs^4*ρ₀ˡ*ρ₀ˢ/ρ₀^2) ) / (2ρ₀^2)
    α  = ρ₀ * α₃ + K/ρ₀^2
 
-   K, α, µ, ρ₀, ρ₀ˢ, ρ₀ˡ
+   Float32.((K, α, µ, ρ₀, ρ₀ˢ, ρ₀ˡ))
 end
 
-# specify force functions F₁ and F₂
-F₁(t, x₁, x₂) = 100exp(-t^2 - x₁^2 - x₂^2)
-F₂(t, x₁, x₂) = 1000exp(-t^2 - x₁^2 - x₂^2)
+# Specify force functions F₁ and F₂
+F₁(t, x₁, x₂) = 100exp(-t^2 - x₁^2 - x₂^2) * sin(t)
+F₂(t, x₁, x₂) = 1000exp(-t^2 - x₁^2 - x₂^2) * cos(t)
 
-# specify variable intervals
-tspan  = IntervalDomain(0.0, 60.0)
-x₁span = IntervalDomain(-100.0, 100.0)
-x₂span = IntervalDomain(0.0, 100.0)
+# Specify variable intervals
+ tspan = IntervalDomain(0.f0, 60.f0)
+x₁span = IntervalDomain(-100.f0, 100.f0)
+x₂span = IntervalDomain(0.f0, 100.f0)
 
-# specify discretization
-Δt  = 20.0
-Δx₁ = 40.0
-Δx₂ = 25.0
+# Specify discretization
+Δt  = 10.f0
+Δx₁ = 20.f0
+Δx₂ = 10.f0
 
-# find solution to the PDE by fitting the neural network to dynamics
-sol = fitPDESystem!(K, α, µ, ρ₀, ρ₀ˢ, ρ₀ˡ,
-                    F₁, F₂,
-                    tspan, x₁span, x₂span,
-                    Δt, Δx₁, Δx₂)
+# Fit the neural network to the PDE system
+w = fitPDESystem!(K, α, µ, ρ₀, ρ₀ˢ, ρ₀ˡ,
+                  F₁, F₂,
+                  tspan, x₁span, x₂span,
+                  Δt, Δx₁, Δx₂)
 
-sol(tspan.lower, x₁span.lower, x₂span.lower)
+w(tspan.lower, x₁span.lower, x₂span.lower)
